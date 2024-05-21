e697d806395e29c70de9cbdfff25bf5d1af51491
PhoeuySophy <146904081+PhoeuySophy@users.noreply.github.com>
2024-05-21 20:17:24 +0700

    Add files via upload

---
+++ Repository/ICustomerService.cs
@@ -0,0 +1,15 @@
+﻿
+using FormCustomer.Models;
+
+namespace FormCustomer.Repository
+
+{
+    public interface ICustomerService
+    {
+        IEnumerable<Customer> GetAll();
+        Customer GetById(int id);
+        bool Create(Customer customer);
+        bool Update(Customer customer);
+        bool Delete(int CustomerId);
+    }
+}
